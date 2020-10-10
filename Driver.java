84ac4ac1ada2e8b71e2a5cb4a4946f47a83663f1
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-10 15:37:36 +0100

    Added the Earth and the Moon. Orbits now all functional

--- Driver.java
+++ Driver.java
@@ -1,10 +1,20 @@
 public class Driver {
     public static void main(String[] args) {
-        // Main Function
-
-        // Create an instance of the Solar System
-        SolarSystem space = new SolarSystem(600,600);
-        // Draw a Sun in the middle of the window
-        space.drawSolarObject(50, 0, 100, "YELLOW");
+        // Create an instance of Solar System to create a window 
+        SolarSystem space = new SolarSystem(500,500);
+        int angleEarth = 0;
+        int angleMoon = 0;
+        
+        while (true) {
+        // Draw Sun in the middle of the "Space" window 
+        space.drawSolarObject(0, 0, 90, "YELLOW");
+        // Draw Earth in orbit from the Sun 
+        space.drawSolarObject(120, angleEarth++, 40, "BLUE");
+        // Draw Moon in orbit of the Earth
+        space.drawSolarObjectAbout(120,angleEarth, 20, "WHITE", 40, angleMoon+=5);
+        // Update the "Space" window
+        space.finishedDrawing();
+        }
+        
     }
 }
