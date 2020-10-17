219e9ba31f580237e35e04eefd547399cf81562e
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-17 12:44:26 +0100

    Adding a new Class as a superclass to all the objects put on the window including a constructor method

---
+++ SolarObjects.java
@@ -0,0 +1,10 @@
+public class SolarObjects {
+    // Instance Variables 
+    public String color;
+    public int angle;
+    public int diameter;
+    public int distance;
+    public int veloctiy;
+    
+
+}
