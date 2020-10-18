d9b287a494d2a559686936ac10a7505fcae7a0be
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-18 15:18:50 +0100

    Added Instance Variables & a few methods

--- SolarObjects.java
+++ SolarObjects.java
@@ -1,15 +1,23 @@
 public class SolarObjects {
+    
     // Instance Variables 
+    public String objectType;
+    public String color;
     public int distance;
     public int diameter;
-
-    public SolarObjects(int distance, int diameter) {
-
+    public int angle;
+    public int velocity;
+    
+    public SolarObjects(String objectType, String color, int distance, int diameter, int angle, int velocity) {
+        
+        this.objectType = objectType;
+        this.color = color;
         this.distance = distance;
         this.diameter = diameter;
-       
-        
+        this.angle = angle;
     }
+
+    public String getObjectType() {return objectType;}
 
     
 
