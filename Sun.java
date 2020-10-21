e371b2617e8a86c79e9f1d5e4db9d8f32a78d9e3
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-21 11:47:00 +0100

    Removed Get methods

--- Sun.java
+++ Sun.java
@@ -1,19 +1,10 @@
 public class Sun extends SolarObjects{
     
-    public int sunAngle;
-    public int sunDiameter;
-    public int sunDistance;
-    public String sunColor;
-    public Sun(int sunDistance ,int sunDiameter, int sunAngle){
-        super("SUN", "YELLOW", sunDistance, sunDiameter,sunAngle); 
-        sunColor = "YELLOW";
+   
+    public Sun(String sunColor, int sunDistance ,int sunDiameter, int sunAngle){
+        super(sunColor, sunDistance, sunDiameter,sunAngle); 
+        
     }
 
-    public int getDistance(){ return sunDistance;}
-
-    public int getDiameter(){return sunDiameter;}
-
-    public int getAngle(){return sunAngle;}
-
-    public String getColor() {return sunColor;}
+   
 }
