c87d696849ab853efa7ffa4bb5ad53c578fb19aa
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-21 10:10:16 +0100

    Changing the Planets Constructor, removing the instance variables which is already in the super class. FIX the Driver.java class so that all the planets are in a SolarObject Array - Display on the window

--- Planets.java
+++ Planets.java
@@ -1,19 +1,20 @@
-public class Planets extends SolarObjects{
+public class Planets extends Sun{
     
     public int planetVelocity;
-    public int planetDistance;
-    public int planetRotationAngle;
-    public int planetDiameter;
-    public String planetColor;
+    public int planetCentreRotationDistance;
+    public int planetCentreRotationAngle;
 
-    public Planets(int planetDistance, int planetRotationAngle, int planetDiameter, String planetColor, int planetVelocity) {
-        super("PLANET", planetColor, planetDistance, planetDiameter, planetRotationAngle);
+    public Planets(int planetDistance, int planetCentreRotationDistance, int planetAngle, int planetCentreRotationAngle, int planetDiameter, String planetColor, int planetVelocity) {
+        super(planetColor, planetDistance, planetDiameter, planetAngle);
         this.planetVelocity = planetVelocity;
-        this.planetDistance = planetDistance;
-        this.planetRotationAngle = planetRotationAngle;
-        this.planetDiameter = planetDiameter;
-        this.planetColor = planetColor;
+        this.planetCentreRotationDistance = planetCentreRotationDistance;
+        this.planetCentreRotationAngle = planetCentreRotationAngle;
         
-
     }
+
+    public int getVelocity() {return planetVelocity;}
+
+    public int getCentreRotationAngle() {return planetCentreRotationAngle;}
+
+    public int getCentreRotationDistance() {return planetCentreRotationDistance;}
 }
