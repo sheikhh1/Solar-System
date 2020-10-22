4bd3a963798d9df03b686a226d29f6f080600101
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-22 11:13:59 +0100

    Finished the Moon Class. Added CentreOfRotationDistance & CentreOfRotationAngle as 2 new instance variables part of the MoonClass. PlanetSpeed was also added as the angle of the moon should be keeping up with the planet it is orbiting. The class also has some get and set methods. Move Method to move the moon so it orbits a planet

---
+++ Moons.java
@@ -0,0 +1,25 @@
+public class Moons extends Planets {
+
+    public double moonCentreOfRotationAngle;
+    public int moonCentreOfRotationDistance;
+    public double planetSpeed;
+    public Moons(int moonDistance, double moonAngle, int moonDiameter, String moonColor, int moonCentreOfRotationDistance, double moonCentreOfRotationAngle, double moonSpeed, double planetSpeed){
+        super(moonDistance,moonAngle,moonDiameter,moonColor,moonSpeed);
+        this.moonCentreOfRotationAngle = moonCentreOfRotationAngle;
+        this.moonCentreOfRotationDistance  = moonCentreOfRotationDistance;
+        this.planetSpeed = planetSpeed;
+        //setDistance(moonDistance);
+    }
+
+    public double getCentreOfRotationAngle(){return moonCentreOfRotationAngle;}
+
+    public int getCentreOfRotationDistance(){return moonCentreOfRotationDistance;}
+
+    public void setCentreOfRotationAngle(double i){moonCentreOfRotationAngle = i;}
+
+    public void move() {
+        setAngle(getAngle() + planetSpeed);
+        setCentreOfRotationAngle(getCentreOfRotationAngle() +getVelocity());
+
+    }
+}
