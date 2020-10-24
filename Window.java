218bbfeac2c8def080c4e490b091c923656e364f
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-24 11:27:31 +0100

    Added all the functionality for the planets sun and moon. Randomised Moons and Asteroids appear

--- Window.java
+++ Window.java
@@ -1,12 +1,15 @@
+import java.util.Random;
 
 public class Window {
-    SolarObjects[] spaceObjects = new SolarObjects[15];
+    SolarObjects[] spaceObjects = new SolarObjects[250];
     SolarSystem space = new SolarSystem(1100,1100);
+    Random rand = new Random();
 
     public Window() {
         addSun();
         addPlanets();
         addMoons();
+        addAsteroids();
         run();
     }
     public void addSun() {
@@ -18,7 +21,7 @@ public class Window {
         spaceObjects[2] = new Planets(120,0,15,"WHITE",-4.9); // Venus
         spaceObjects[3] = new Planets(180,0,15,"#3644E4",-4.1); //Earth
         spaceObjects[4] = new Planets(240,0,20,"RED", -3.4); // Mars
-        spaceObjects[5] = new Planets(310,0,30,"ORANGE",-1.8); // Jupiter
+        spaceObjects[5] = new Planets(320,0,25,"ORANGE",-1.8); // Jupiter
         spaceObjects[6] = new Planets(390,0,20,"#AB604A", -1.35); // Saturn
         spaceObjects[7] = new Planets(450,0,18,"#ACE5EE",-1); // Uranus 
         spaceObjects[8] = new Planets(510,0,17,"#4B70DD",-0.8); // Neptune
@@ -26,13 +29,27 @@ public class Window {
 
     public void addMoons(){
         int x =0;
-        spaceObjects[9] = new Moons(180,0,10, "LIGHT_GRAY", 20,0,-6,-4.1); // Earth Moon
-        spaceObjects[10] = new Moons(240,0,12, "LIGHT_GRAY", 20,0,-6,-3.4); // Mars Moon 1 (Phobos)
-        spaceObjects[11] = new Moons(240,0,10, "LIGHT_GRAY", 20,200,-6,-3.4); // Mars Moon 2 (Deimos)
+        spaceObjects[9] = new Moons(180,0,5, "LIGHT_GRAY", 20,0,-6,-4.1); // Earth Moon
+        spaceObjects[10] = new Moons(240,0,7, "LIGHT_GRAY", 20,0,-6,-3.4); // Mars Moon 1 (Phobos)
+        spaceObjects[11] = new Moons(240,0,5, "LIGHT_GRAY", 20,200,-6,-3.4); // Mars Moon 2 (Deimos)
 
         //Jupiters Galilean Moons
         for (int i = 12; i < 15; i++) {
-            spaceObjects[i] = new Moons(310,0,10,"LIGHT_GRAY", 30,x+=120,-6,-1.8);
+            spaceObjects[i] = new Moons(320,0,5,"LIGHT_GRAY", 25,x+=120,-6,-1.8);
+        }
+
+        // Saturn Ring - 82 Moons
+        for (int i = 15; i < 97; i++) {
+            int y = rand.nextInt(360) + 1;
+            spaceObjects[i] = new Moons(390,0,5,"LIGHT_GRAY", 25,y,-6,-1.35);
+        }
+    }
+
+    public void addAsteroids(){
+        for (int i = 97; i < 250; i++) {
+            int asteroidAngle = rand.nextInt(360);
+            int asteroidDistance = (int) ((Math.random() * (277 - 274)) + 274);
+            spaceObjects[i] = new Asteroids(asteroidDistance,asteroidAngle,5,"LIGHT_GRAY",-6);
         }
     }
 
@@ -40,26 +57,23 @@ public class Window {
         while (true) {
             space.drawSolarObject(spaceObjects[0].getDistance(), spaceObjects[0].getAngle(), spaceObjects[0].getDiameter(), spaceObjects[0].getColor()); // Add SUN
 
-            for (int i = 1; i < 9; i++){
-
-                space.drawSolarObject(spaceObjects[i].getDistance(),spaceObjects[i].getAngle(), spaceObjects[i].getDiameter(), spaceObjects[i].getColor());
+        for (int i = 1; i < 9; i++){
+            space.drawSolarObject(spaceObjects[i].getDistance(),spaceObjects[i].getAngle(), spaceObjects[i].getDiameter(), spaceObjects[i].getColor());
         }
 
-        for (int i = 9; i < spaceObjects.length; i++){
+        for (int i = 9; i < 97; i++){
             space.drawSolarObjectAbout(spaceObjects[i].getDistance(), spaceObjects[i].getAngle(), spaceObjects[i].getDiameter(), spaceObjects[i].getColor(), ((Moons)spaceObjects[i]).getCentreOfRotationDistance(), ((Moons)spaceObjects[i]).getCentreOfRotationAngle());
-        
+        }
 
+        for (int i = 97; i < 250; i++) {
+            space.drawSolarObject(spaceObjects[i].getDistance(), spaceObjects[i].getAngle(), spaceObjects[i].getDiameter(), spaceObjects[i].getColor());
         }
           
         space.finishedDrawing();
 
         for (int i = 1; i < spaceObjects.length; i++) {
             spaceObjects[i].move();
-        }
-
-        
-
-            
+        } 
         }
     }
 }
