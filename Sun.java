d1e65e8ac1856efbe547dbe4972502566e78e2be
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-24 11:28:18 +0100

    Testing

--- Sun.java
+++ Sun.java
@@ -1,6 +1,5 @@
 public class Sun extends SolarObjects{
     
-   
     public Sun(String sunColor, int sunDistance ,int sunDiameter, double sunAngle){
         super(sunColor, sunDistance, sunDiameter,sunAngle); 
         
