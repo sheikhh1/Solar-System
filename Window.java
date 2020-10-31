1958fd1ed54b3a87949e873c405efc370e831993
sheikhh1 <haarissheikh2000@gmail.com>
2020-10-31 14:42:52 +0000

    Made the code a little more elegant by creating diameter,distance,colors,speed arrays so only 1 for loop is called when adding the planets rather than manually typing out each planets data. Finished Commenting the code

--- Window.java
+++ Window.java
@@ -1,9 +1,13 @@
 import java.util.Random;
 
 public class Window {
-    SolarObjects[] spaceObjects = new SolarObjects[250];
-    SolarSystem space = new SolarSystem(1100,1100);
-    Random rand = new Random();
+    private SolarObjects[] spaceObjects = new SolarObjects[250];
+    private SolarSystem space = new SolarSystem(1100,1100);
+    private Random rand = new Random();
+    private int [] planetDistance = new int[] {65,120,180,240,320,390,450, 510};
+    private int [] planetDiameter = new int[] {10,15,15,20,25,20,18,17};
+    private String[] planetColors = new String[] {"DARK_GRAY", "WHITE", "#3644E4", "RED", "ORANGE", "#AB604A", "#ACE5EE","#4B70DD"};
+    private double[] planetSpeeds = new double[] {-6.3, -4.4, -3.7, -3.1, -1.6, -1.1, -0.8, -0.5};
 
     public Window() {
         addSun();
@@ -17,39 +21,37 @@ public class Window {
     }
 
     public void addPlanets() {
-        spaceObjects[1] = new Planets(65,0,10,"DARK_GRAY",-7); //Mecury
-        spaceObjects[2] = new Planets(120,0,15,"WHITE",-4.9); // Venus
-        spaceObjects[3] = new Planets(180,0,15,"#3644E4",-4.1); //Earth
-        spaceObjects[4] = new Planets(240,0,20,"RED", -3.4); // Mars
-        spaceObjects[5] = new Planets(320,0,25,"ORANGE",-1.8); // Jupiter
-        spaceObjects[6] = new Planets(390,0,20,"#AB604A", -1.35); // Saturn
-        spaceObjects[7] = new Planets(450,0,18,"#ACE5EE",-1); // Uranus 
-        spaceObjects[8] = new Planets(510,0,17,"#4B70DD",-0.8); // Neptune
+
+        for (int i = 1; i < 9; i++) {
+            spaceObjects[i] = new Planets(planetDistance[i-1],0,planetDiameter[i-1],planetColors[i-1], planetSpeeds[i-1]);
+        }
     }
 
     public void addMoons(){
         int x =0;
-        spaceObjects[9] = new Moons(180,0,5, "LIGHT_GRAY", 20,0,-6,-4.1); // Earth Moon
-        spaceObjects[10] = new Moons(240,0,7, "LIGHT_GRAY", 20,0,-6,-3.4); // Mars Moon 1 (Phobos)
-        spaceObjects[11] = new Moons(240,0,5, "LIGHT_GRAY", 20,200,-6,-3.4); // Mars Moon 2 (Deimos)
+        spaceObjects[9] = new Moons(180,0,5, "LIGHT_GRAY", 20,0,-6,planetSpeeds[2]); // Earth Moon
+        spaceObjects[10] = new Moons(240,0,7, "LIGHT_GRAY", 20,0,-6,planetSpeeds[3]); // Mars Moon 1 (Phobos)
+        spaceObjects[11] = new Moons(240,0,5, "LIGHT_GRAY", 20,200,-6,planetSpeeds[3]); // Mars Moon 2 (Deimos)
 
         //Jupiters Galilean Moons
         for (int i = 12; i < 15; i++) {
-            spaceObjects[i] = new Moons(320,0,5,"LIGHT_GRAY", 25,x+=120,-6,-1.8);
+            spaceObjects[i] = new Moons(320,0,5,"LIGHT_GRAY", 25,x+=120,-6,planetSpeeds[4]);
         }
 
         // Saturn Ring - 82 Moons
         for (int i = 15; i < 97; i++) {
             int y = rand.nextInt(360) + 1;
-            spaceObjects[i] = new Moons(390,0,5,"LIGHT_GRAY", 25,y,-6,-1.35);
+            spaceObjects[i] = new Moons(390,0,5,"LIGHT_GRAY", 25,y,-6,planetSpeeds[5]);
         }
     }
 
     public void addAsteroids(){
+        int asteroidSpeed = (int) ((Math.random() * (10 - 1)) + 1);
         for (int i = 97; i < 250; i++) {
+            if (i % 2 > 0) asteroidSpeed *= -1;
             int asteroidAngle = rand.nextInt(360);
-            int asteroidDistance = (int) ((Math.random() * (277 - 274)) + 274);
-            spaceObjects[i] = new Asteroids(asteroidDistance,asteroidAngle,5,"LIGHT_GRAY",-6);
+            int asteroidDistance = (int) ((Math.random() * (278 - 273)) + 274);
+            spaceObjects[i] = new Asteroids(asteroidDistance,asteroidAngle,5,"LIGHT_GRAY",asteroidSpeed);
         }
     }
 
